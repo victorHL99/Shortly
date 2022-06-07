88a6f7b75ed43bab998b89c8cdc59eed5c66afb3
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-07 11:14:38 -0300

    feat: import middlewares for userRoutes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,10 @@
 import {Router} from 'express';
 
+//Imports controllers
 import {registerUser} from '../controllers/userController.js';
+
+//Imports Middlewares
+import {vPostRegisterUser} from '../middlewares/verifiersMiddlewares.js';
 
 const userRouter = Router();
 
