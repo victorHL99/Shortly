94a8b6231c002c915c731a9bad04dfdd0824face
victorHL99 <victor.lessa@estudante.ufla.br>
2022-06-10 14:06:45 -0300

    <fix> : arrumando bug no ranking v4

--- controllers/rankingController.js
+++ controllers/rankingController.js
@@ -5,7 +5,7 @@ export async function showRanking(req, res) {
         const resultUsersForRanking = await db.query(`
             SELECT u.id, u.name, 
             COUNT(l."shortlyLink") AS "linksCount", 
-            SUM(l."views") AS "viewsMax"
+            COALESCE(SUM(l."views")) AS "viewsMax"
             FROM users u 
             LEFT JOIN links l
             ON u.id = l."creatorId"
@@ -20,11 +20,11 @@ export async function showRanking(req, res) {
                 "id": user.id,
                 "name": user.name,
                 "linksCount": user.linksCount,
-                "visitCount": user.viewsMax,
+                "visitCount": ((user.viewsMax === null) ? 0 : user.viewsMax),
             }
         }
         )
-        res.status(200).send(resultUsersForRanking.rows);
+        res.status(200).send(arrayUsersForRanking);
     } catch (error) {
         res.sendStatus(500);
     }
